1e0bed84925ac3f189af4d37e0ed173d7171bcad
zhouhua <709653949@qq.com>
2014-05-01 22:34:19 +0800

    asyncmysql pass

--- examples/async_mysql2.php
+++ examples/async_mysql2.php
@@ -3,14 +3,24 @@ if(!function_exists('swoole_get_mysqli_sock')) {
 	die("no async_mysql support\n");
 }
 $db = new mysqli;
-$db->connect('127.0.0.1', 'root', 'root', 'test');
-$db->query("show tables", MYSQLI_ASYNC);
+$db->connect('127.0.0.1', 'root', '123456', 'paopaogame');
+$sql="show tables";
+$db->query($sql, MYSQLI_ASYNC);
+$readinfo="read mysql back:".$sql;
+$count=0;
 swoole_event_add(swoole_get_mysqli_sock($db), function($__db_sock) {
     global $db;
-    var_dump($__db_sock);
+    global $readinfo;
+    global $count;
+    global $sql;
+    
+//     var_dump($__db_sock);
     $res = $db->reap_async_query();
     var_dump($res->fetch_all(MYSQLI_ASSOC));
-    $db->query("show tables", MYSQLI_ASYNC);
+    $db->query("SELECT * FROM paopaogame.paopaoaccount limit 0,2;", MYSQLI_ASYNC);
+    echo $readinfo." count=".$count."\n";
+    $count++;
+//     echo "sleep "."1"." second";
     sleep(1);
 });
 echo "Finish\n";
