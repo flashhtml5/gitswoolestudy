2cba183daf04c56cc855382386cfabfa8f1e8418
zhouhua <709653949@qq.com>
2014-05-02 11:26:45 +0800

    taskwait return ok

--- examples/task.php
+++ examples/task.php
@@ -1,5 +1,5 @@
 <?php
-$serv = new swoole_server("127.0.0.1", 9501);
+$serv = new swoole_server("0.0.0.0", 10000);
 $serv->set(array(
     'worker_num' => 1,
     'task_worker_num' => 1,
@@ -7,26 +7,25 @@ $serv->set(array(
 $serv->on('Receive', function(swoole_server $serv, $fd, $from_id, $data) {
 	//AsyncTask
     //$data = trim($data);
-    $data = str_repeat('A', 8192*100);
+    $data = str_repeat($data.'\n', 10);
     //if($data == 'async')
     if (false)
     {
-        $task_id = $serv->task($data);
         echo "Dispath AsyncTask: id=$task_id\n";
+        $task_id = $serv->task($data);
     }
     //Sync Task
 	else
     {
+        echo "Dispath SyncTask: data_len=".strlen($data).PHP_EOL;       
         $res = $serv->taskwait($data);
-        echo "Dispath SyncTask: data_len=".strlen($res).PHP_EOL;       
     }
-    $serv->send($fd, "OK\n");
+    $serv->send($fd, $res."\n");
 });
-$serv->on('Task', function (swoole_server $serv, $task_id, $from_id, $data) {
+$serv->on('Task', function ($serv, $task_id, $from_id, $data) {
 
-    echo "AsyncTask[PID=".posix_getpid()."]: task_id=$task_id, data_len=".strlen($data).".".PHP_EOL;
-    $serv->finish($data);
-    return;
+    echo "AsyncTask[PID=".posix_getpid()."] Taskstart: task_id=$task_id, data_len=".strlen($data).".".PHP_EOL;
+   	return $data;
     
     $start_fd = 0;
 	while(true)
@@ -42,7 +41,8 @@ $serv->on('Task', function (swoole_server $serv, $task_id, $from_id, $data) {
 			$serv->send($fd, "AsyncTask: hello\n");
 		}
 	}
-    $serv->finish("Task:[$data] -> OK\n");
+//     $serv->finish("Task:[$data] -> OK\n");
+    return "Task:[$data] -> OK\n";
 });
 $serv->on('Finish', function (swoole_server $serv, $task_id, $data) {
     echo "AsyncTask[$task_id] Finish: data_len=".strlen($data).PHP_EOL;
